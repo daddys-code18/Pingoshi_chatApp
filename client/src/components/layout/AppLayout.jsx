802593e388aca4d6426bb463f6e19183a3f1240d
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-03-13 05:03:05 +0530

    GridLayout

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,13 +1,34 @@
-import Title from "../shared/Title";
+// import Title from "../shared/Title";
 import Header from "./Header";
+import { Grid } from '@mui/material';
 
 const AppLayout = () => (WrappedComponent) => {
+
     return (props) => {
         return (
             <>
                 {/* <Title /> */}
                 <Header />
-                <WrappedComponent {...props} />
+
+                <Grid container height={"calc(100vh - 4rem)"}>
+                    <Grid item sm={4} md={3} sx={{
+                        display: { xs: "none", sm: "block" }
+                    }} height={"100%"}>Frist</Grid>
+
+                    <Grid item xs={12} md={5} lg={6}
+                        height={"100%"}>
+                        <WrappedComponent {...props} />
+
+                    </Grid>
+
+                    <Grid item md={4} lg={3} sx={{
+                        display: { xs: "none", md: "block" },
+                        padding: "2rem",
+                        bgcolor: "rgba(0,0,0,0.85)"
+                    }} height={"100%"}>Third</Grid>
+
+                </Grid>
+
                 <div>
                     Footer
                 </div>
