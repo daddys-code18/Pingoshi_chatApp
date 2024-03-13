473115d55de02dca3527c71f82e01da2ab208db9
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-03-14 03:34:44 +0530

    trycatch error handling

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express, { urlencoded } from "express";
 import userRoute from "./routes/userRoutes.js";
 import { connectDB } from "./utils/features.js";
 import dotenv from "dotenv";
+import { errorMiddleware } from "./middleware/errorMiddleware.js";
 
 dotenv.config({
   path: "./.env",
@@ -19,6 +20,7 @@ app.use("/user", userRoute);
 app.get("/", (req, res) => {
   res.send("hello World");
 });
+app.use(errorMiddleware);
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,10 @@
 import { compare } from "bcrypt";
 import { User } from "../models/userModel.js";
 import { sendToken } from "../utils/features.js";
+import { TryCatch } from "../middleware/errorMiddleware.js";
+import { ErrorHandler } from "../utils/utility.js";
 
-export const newUser = async (req, res) => {
+export const newUser = TryCatch(async (req, res) => {
   const { name, username, password, bio } = req.body;
   const avatar = {
     public_id: "sfsdfs",
@@ -11,14 +13,15 @@ export const newUser = async (req, res) => {
   const user = await User.create({ name, password, username, avatar, bio });
 
   sendToken(res, user, 201, "User created");
-};
+});
 
-export const login = async (req, res) => {
+export const login = TryCatch(async (req, res, next) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username }).select("+password");
-  if (!user) return res.status(400).json({ message: "Invalid Username" });
+  if (!user) return next(new ErrorHandler("Invalid UserName or Password", 404));
 
   const isMatch = await compare(password, user.password);
-  if (!isMatch) return res.status(400).json({ message: "Invalid Password" });
+  if (!isMatch)
+    return next(new ErrorHandler("Invalid UserName or Password", 404));
   sendToken(res, user, 200, `Welcome ${user.username}`);
-};
+});
