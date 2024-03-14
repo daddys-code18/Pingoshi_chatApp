dd38fd11d18a8885f06d4d53d6004e2f3d41691b
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-03-14 05:59:40 +0530

    My Chat

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -3,6 +3,7 @@ import { ErrorHandler } from "../utils/utility.js";
 import { Chat } from "../models/chatModel.js";
 import { emitEvent } from "../utils/features.js";
 import { ALERT, REFETCH_CHATS } from "../constants/event.js";
+import { getOtherMember } from "../lib/helper.js";
 
 export const newGroupChat = TryCatch(async (req, res, next) => {
   const { name, members } = req.body;
@@ -25,24 +26,31 @@ export const newGroupChat = TryCatch(async (req, res, next) => {
     message: "Group  Created",
   });
 });
-export const getMyChat = TryCatch(async (req, res, next) => {
-  const { name, members } = req.body;
 
-  if (members.length < 2)
-    return next(
-      new ErrorHandler("Group chat must have at least 3 members", 400)
-    );
-  const allMembers = [...members, req.user];
-  await Chat.create({
-    name,
-    groupChat: true,
-    creator: req.user,
-    members: allMembers,
+export const getMyChat = TryCatch(async (req, res, next) => {
+  const chats = await Chat.find({ members: req.user }).populate(
+    "members",
+    "name avatar"
+  );
+  const transformedChats = chats.map(({ _id, name, members, groupChat }) => {
+    const otherMember = getOtherMember(members, req.user);
+    return {
+      _id,
+      groupChat,
+      avatar: groupChat
+        ? members.slice(0, 3).map(({ avatar }) => avatar.url)
+        : [otherMember.avatar.url],
+      name: groupChat ? name : otherMember.name,
+      members: members.reduce((prev, curr) => {
+        if (curr._id.toString() !== req.user.toString()) {
+          prev.push(curr._id);
+        }
+        return prev;
+      }, []),
+    };
   });
-  emitEvent(req, ALERT, allMembers, `Welcome to${name} group`);
-  emitEvent(req, REFETCH_CHATS, members);
   return res.status(201).json({
     success: true,
-    message: "Group  Created",
+    chats: transformedChats,
   });
 });
