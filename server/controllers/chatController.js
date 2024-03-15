38d74bcf030aa373e3d9ea70645e16ca483f0174
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-03-16 01:08:07 +0530

    Add members in group

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -4,6 +4,7 @@ import { Chat } from "../models/chatModel.js";
 import { emitEvent } from "../utils/features.js";
 import { ALERT, REFETCH_CHATS } from "../constants/event.js";
 import { getOtherMember } from "../lib/helper.js";
+import { User } from "./../models/userModel.js";
 
 export const newGroupChat = TryCatch(async (req, res, next) => {
   const { name, members } = req.body;
@@ -52,5 +53,63 @@ export const getMyChat = TryCatch(async (req, res, next) => {
   return res.status(201).json({
     success: true,
     chats: transformedChats,
+  });
+});
+
+export const getMyGroups = TryCatch(async (req, res, next) => {
+  const chats = await Chat.find({
+    members: req.user,
+    groupChat: true,
+    creator: req.user,
+  }).populate("members", "name avatar");
+  const groups = chats.map(({ members, _id, groupChat, name }) => ({
+    _id,
+    groupChat,
+    name,
+    avatar: members.slice(0, 3).map(({ avatar }) => avatar.url),
+  }));
+  return res.status(200).json({
+    success: true,
+    groups,
+  });
+});
+
+export const addMembers = TryCatch(async (req, res, next) => {
+  const { chatId, members } = req.body;
+
+  if (!members || members.length < 1)
+    return next(new ErrorHandler("Please provide members", 400));
+
+  const chat = await Chat.findById(chatId);
+  if (!chat) return next(new ErrorHandler("Chat Not Found", 404));
+
+  if (!chat.groupChat)
+    return next(new ErrorHandler("This is not a group chat"));
+
+  if (chat.creator.toString() !== req.user.toString())
+    return next(new ErrorHandler("You are not allowed to add members", 403));
+
+  const allNewMembersPromise = members.map((i) => User.findById(i, "name"));
+
+  const allNewMembers = await Promise.all(allNewMembersPromise);
+
+  const uniqueMembers = allNewMembers
+    .filter((i) => !chat.members.includes(i._id.toString()))
+    .map((i) => i._id);
+
+  chat.members.push(...uniqueMembers);
+
+  if (chat.members.length > 100)
+    return next(new ErrorHandler("Group members limit reached"));
+  await chat.save();
+
+  const allUserName = allNewMembers.map((i) => i.name).join(",");
+
+  emitEvent(req, ALERT, chat.members, `${allUserName} has been added in group`);
+  emitEvent(req, REFETCH_CHATS, chat.members);
+
+  return res.status(200).json({
+    success: true,
+    message: "Members add Successfully",
   });
 });
--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -2,10 +2,17 @@ import express from "express";
 
 import { multerUpload, singleAvatar } from "../middleware/multer.js";
 import { isAuthenticated } from "../middleware/auth.js";
-import { getMyChat, newGroupChat } from "../controllers/chatController.js";
+import {
+  addMembers,
+  getMyChat,
+  getMyGroups,
+  newGroupChat,
+} from "../controllers/chatController.js";
 const app = express.Router();
 app.use(isAuthenticated);
 app.post("/new", newGroupChat);
 app.get("/my", getMyChat);
+app.get("/groups", getMyGroups);
+app.put("/addmembers", addMembers);
 
 export default app;
