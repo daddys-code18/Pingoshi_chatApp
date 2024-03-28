bca3869c747b9077ce4aa5fa9397a220dbeffafe
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-03-28 13:52:17 +0530

    avatar

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,10 +1,21 @@
 // import Title from "../shared/Title";
+import { useParams } from "react-router-dom";
+import { sampleChats } from "../../constants/sampleData";
+import ChatList from "../specific/ChatList";
 import Header from "./Header";
 import { Grid } from '@mui/material';
 
 const AppLayout = () => (WrappedComponent) => {
 
     return (props) => {
+        const params = useParams()
+        const chatId = params.chatId
+
+        const handleDeleteChart = (e, _id, groupChat) => {
+            e.preventDefault();
+            console.log("Delete Chat", _id, groupChat)
+        }
+
         return (
             <>
                 {/* <Title /> */}
@@ -13,7 +24,9 @@ const AppLayout = () => (WrappedComponent) => {
                 <Grid container height={"calc(100vh - 4rem)"}>
                     <Grid item sm={4} md={3} sx={{
                         display: { xs: "none", sm: "block" }
-                    }} height={"100%"}>Frist</Grid>
+                    }} height={"100%"}>
+                        <ChatList chats={sampleChats} chatId={chatId} handleDeleteChart={handleDeleteChart}
+                        /></Grid>
 
                     <Grid item xs={12} md={5} lg={6}
                         height={"100%"}>
