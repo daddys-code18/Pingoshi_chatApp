df1b72823a6bff246538115950d808ce52f63187
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-03-28 20:34:56 +0530

    Home Component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import AppLayout from "../components/layout/AppLayout"
+import { Box, Typography } from '@mui/material';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <Box bgcolor={"rgba(0,0,0,0.1)"} height={"100%"}>
+            <Typography p={"2rem"} variant="h5" textAlign={"center"}>Select a friend to Chat</Typography>
+
+        </Box>
     )
 }
 
